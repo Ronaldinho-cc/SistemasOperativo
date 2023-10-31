6f4890f8b65cb8c424d664d4c3c9fa6cbf2054b8
RonaldinhoCc <ronaldinho.ccencho@vallegrande.edu.pe>
2023-10-31 16:23:30 -0500

    node.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 const path = require('path');
 
-app.use('/public', express.static(path.join(__dirname, '/public')))
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.send('Conociendo Node.js');
