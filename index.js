98e18d66cf0120758517cd7ba8bb11cf7f95f843
RonaldinhoCc <ronaldinho.ccencho@vallegrande.edu.pe>
2023-11-02 18:59:33 -0500

    version

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = express()
 const port = 3000
 const path = require('path');
 
-app.use('/public', express.static(path.join(__dirname, 'public')))
-
-app.get('/', (req, res) => {
+app.get('/', (req, res) =>{
     res.send('Conociendo Node.js');
-})
+});
+
+/*app.get('/public', (req, res) => {
+    var filePath = path.join(__dirname,'/public/index.html');
+    res.sendFile(filePath);
+})*/
 
 app.listen(port, () => {
     console.log(`Se esta ejecutando sobre el puerto ${port}`)
